36b2722c93100ca476afa19857191b04ee6f44bc
Bram Moreinis <bmoreinis@gmail.com>
2021-04-21 20:45:10 +0000

    Added table suggestion

--- scripts/tables.js
+++ scripts/tables.js
@@ -1,3 +1,48 @@
+/* Get FactorList */
+// Analyzes mistakes and returns the times table to study
+function getStudyTable() {
+  var mistakes = [[3,4],[3,6],[7,4],[4,6],[6,4],[6,7]];
+  // var mistakes = localStorage.getItem("mistakes");
+  var factorList=[];
+  for (var factor = 0; factor < mistakes.length; factor++) {
+    factorList.push(mistakes[factor][0]);
+    factorList.push(mistakes[factor][1]);
+  }
+  factorList = factorList.sort();
+  factorList = factorList.reverse();
+  var counts = []; // array of counts of items in order
+  var dupes = 0; // number of dupes so far
+  var mostFrequent; // most frequent item
+  var studyTable = findFactor(factorList,counts,dupes);
+  return studyTable;
+}
+
+/* StudyList */
+// returns the highst of factors with most duplicates from passed array 
+function findFactor(array,counts,dupes){
+  // Iterate over all items in array
+  for(var item = 0; item < array.length; item++){
+    // look at the next item
+    var factor = array[item];
+    // have we counted this factor before? 
+    if(counts[factor] == undefined) {
+      // count it, so counts[3] will be number of 3s
+      counts[factor] = 1;
+    }
+    else {
+      // add one to that count
+      counts[factor] += 1;
+    }
+    // does this factor have the most dupes?
+    if(counts[factor] > dupes){
+      dupes = counts[factor];
+      mostFrequent = array[item];
+    }
+  }
+  // now that we're cone, we have the highest, most frequent value
+  return mostFrequent;
+}
+
 /* Times Table Maker */
 function tableMaker(newTable){
   var storedTable = localStorage.getItem("oldTable");
@@ -10,7 +55,7 @@ function tableMaker(newTable){
     table = newTable;
   }
   else {
-    table = storedTable;
+    table = getStudyTable();
   }
   var tableHead = "Times Table for "+table;
   var tableHeader = document.getElementById("tableHeader");
@@ -18,6 +63,8 @@ function tableMaker(newTable){
   for (var factor = 2; factor < 10; factor++) {
     tableBody += table+" * "+factor+" = "+table*factor+"<br />";
   }
+  var customMessage = document.getElementById("customMessage");
+  customMessage.innerHTML = "We recommend you study the <span style=\"color:green;\">"+ table +" times table</span> based on your previous errors.";
   var statBox = document.getElementById("stats");
   statBox.innerHTML = tableBody;
   localStorage.setItem("oldTable", 0);
